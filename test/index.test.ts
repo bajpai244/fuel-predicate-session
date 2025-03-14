54a3540f960dd99359cb1a2f4e332622e7c0bdc4
Harsh Bajpai <bajpaiharsh244@gmail.com>
2025-03-14 18:55:04 +0530

    feat: basic predicate test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import { test, describe } from 'bun:test';
-import { hexlify, InputType, max, ScriptRequest, ScriptTransactionRequest, Wallet } from 'fuels';
+import { B512Coder, hexlify, InputType, keccak256, max, ScriptRequest, ScriptTransactionRequest, Wallet } from 'fuels';
 import { launchTestNode } from 'fuels/test-utils';
 import { SessionPredicate } from '../out';
 
@@ -23,13 +23,14 @@ describe('test session predicate', async () => {
     console.log('sessionWallet', sessionWallet.signer().compressedPublicKey.length);
     console.log('sessionWallet', sessionWallet.signer().compressedPublicKey);
 
-    const sessionWalletPublicKey= hexlify(new Uint8Array(Buffer.from(sessionWallet.signer().compressedPublicKey.slice(2), 'hex')).slice(1));
+    // const sessionWalletPublicKey= hexlify(new Uint8Array(Buffer.from(sessionWallet.signer().compressedPublicKey.slice(2), 'hex')).slice(1));
 
     const sessionPredicate = new SessionPredicate({
       configurableConstants: {
         MAIN_ADDRESS: wallet.address.toB256(),
-        SESSION_ADDRESS_PUBLIC_KEY: sessionWalletPublicKey,
+        SESSION_ADDRESS_PUBLIC_KEY: sessionWallet.publicKey,
       },
+      data: [0],
       provider,
     });
 
@@ -53,6 +54,14 @@ describe('test session predicate', async () => {
     scriptTransactionRequest.addResources(resources);
     scriptTransactionRequest.addCoinOutput(randomRecepient, amount, baseAssetId);
 
+    const message = keccak256(new Uint8Array([0,1,2]));
+    const messageHex = hexlify(message);
+
+    console.log('message hex', messageHex);
+    console.log('sign', await sessionWallet.signMessage(messageHex))
+
+    scriptTransactionRequest.addWitness(await sessionWallet.signTransaction(scriptTransactionRequest));
+
     console.log("scriptransaction inputs", scriptTransactionRequest.inputs);
 
     await scriptTransactionRequest.estimateAndFund(sessionWallet);
@@ -62,16 +71,14 @@ describe('test session predicate', async () => {
     sessionPredicate.predicateData = [0];
     sessionPredicate.populateTransactionPredicateData(scriptTransactionRequest);
 
-    if(scriptTransactionRequest.inputs[0].type === InputType.Coin) {
-        // scriptTransactionRequest.inputs[0].predicateGasUsed = maxGas;
-    }
+    // scriptTransactionRequest.witnesses = [];
 
+    console.log('recepient balance before:', await provider.getBalance(randomRecepient, baseAssetId));
 
-    // console.log("scriptransaction inputs", scriptTransactionRequest.inputs);
     const {status} = await (await sessionWallet.sendTransaction(scriptTransactionRequest)).waitForResult();
 
+    console.log('recepient balance after:', await provider.getBalance(randomRecepient, baseAssetId));
+
     console.log("status", status);
-
-
   });
 });
